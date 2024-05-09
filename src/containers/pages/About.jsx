669b027f386440ad1e0e16f9fb16513d307ab611
skyx20 <dc.linkups@gmail.com>
2024-05-09 11:21:03 -0400

    import address bug fixed

--- src/containers/pages/About.jsx
+++ src/containers/pages/About.jsx
@@ -1,33 +1,33 @@
-import Clients from "components/about/Clients";
-import Features from "components/about/Features";
-import Header from "components/about/Header";
-import Images from "components/about/Images";
-import Team from "components/about/Team";
-import TestStats from "components/about/TestStats";
-import Footer from "components/navigation/Footer";
-import Navbar from "components/navigation/Navbar";
-import Layout from "hocs/layout/Layout";
-import {useEffect} from 'react'
+import Clients from "../../components/about/Clients";
+import Features from "../../components/about/Features";
+import Header from "../../components/about/Header";
+import Images from "../../components/about/Images";
+import Team from "../../components/about/Team";
+import TestStats from "../../components/about/TestStats";
+import Footer from "../../components/navigation/Footer";
+import Navbar from "../../components/navigation/Navbar";
+import Layout from "../../hocs/layout/Layout";
+import { useEffect } from 'react'
 
 
 const Cases = () => {
-    useEffect(()=>{
-        window.scrollTo(0,0);
-        }, []);
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
 
-    return (    
-        <Layout> 
-            <Navbar/>
-            <div className="pt-32"> 
-                <Header/>
-                <TestStats/>
-                <Images/>
-                <Clients/>
-                <Features/>
-                <Team/>
+    return (
+        <Layout>
+            <Navbar />
+            <div className="pt-32">
+                <Header />
+                <TestStats />
+                <Images />
+                <Clients />
+                <Features />
+                <Team />
 
             </div>
-            <Footer/>
+            <Footer />
         </Layout>
     );
 }
--- src/containers/pages/Home.jsx
+++ src/containers/pages/Home.jsx
@@ -1,30 +1,30 @@
-import CTA from "components/home/CTA";
-import Features from "components/home/Features";
-import Header from "components/home/Header";
-import Incentives from "components/home/Incentives";
-import LogoCloud from "components/home/LogoCloud";
-import UseCases from "components/home/UseCases";
-import Footer from "components/navigation/Footer";
-import Navbar from "components/navigation/Navbar";
-import Layout from "hocs/layout/Layout";
-import {useEffect} from 'react'
+import CTA from "../../components/home/CTA";
+import Features from "../../components/home/Features";
+import Header from "../../components/home/Header";
+import Incentives from "../../components/home/Incentives";
+import LogoCloud from "../../components/home/LogoCloud";
+import UseCases from "../../components/home/UseCases";
+import Footer from "../../components/navigation/Footer";
+import Navbar from "../../components/navigation/Navbar";
+import Layout from "../../hocs/layout/Layout";
+import { useEffect } from 'react'
 
 const Home = () => {
-    useEffect(()=>{
-        window.scrollTo(0,0);
-        }, []);
-    return (    
-        <Layout> 
-            <Navbar/>
-            <div className="pt-32"> 
-                <Header/> 
-                <Incentives/>
-                <UseCases/>
-                <Features/>
-                <CTA/>
-                <LogoCloud/>
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
+    return (
+        <Layout>
+            <Navbar />
+            <div className="pt-32">
+                <Header />
+                <Incentives />
+                <UseCases />
+                <Features />
+                <CTA />
+                <LogoCloud />
             </div>
-            <Footer/>
+            <Footer />
         </Layout>
     );
 }
